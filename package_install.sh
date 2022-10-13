7283018a1f4224eca3f052446d6f6d83599d1830
Doug Walter <doug.walter@smoothstack.com>
2022-10-13 16:48:17 -0600

    removed python2 install commands

--- package_install.sh
+++ package_install.sh
@@ -1,2 +1,2 @@
-sudo pip3 install -r requirements.txt || sudo pip install -r requirements.txt
-python3 setup.py install || python setup.py install
+sudo pip3 install -r requirements.txt
+python3 setup.py install
