debb2383198c7752c5a2844a5ee0258e0121b821
Doug Walter <doug.walter@smoothstack.com>
2022-10-13 15:17:44 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='reporting_unittest',
+    version='0.0.5',
+    author="Doug Walter",
+    author_email="doug.walter@smoothstack.com",
+    license="Apache 2.0",
+    packages=[
+        'reporting_unittest'
+    ],
+    install_requires=[
+        'selenium'
+    ]
+)
